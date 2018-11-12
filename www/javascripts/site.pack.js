af4be3272cc903e71b747395939f9017adc8629b
walidvb <walidvb@gmail.com>
2018-11-12 11:49:58 +0100

    one-pager

--- www/javascripts/site.pack.js
+++ www/javascripts/site.pack.js
@@ -7,3 +7,21 @@
 // import './fileName'
 //
 // To learn more, visit https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
+
+document.addEventListener('DOMContentLoaded', () => {
+  const lines = document.querySelectorAll('line');
+  function setLines(size){
+    lines.forEach((path) => {
+      const length = path.getTotalLength()
+      path.style.strokeDasharray = length + ' ' + length
+      path.style.strokeDashoffset = length * size
+    })
+  }
+
+  document.addEventListener('scroll', function(evt){
+    var scrollY = window.scrollY;
+    var scrollMax = window.innerHeight*.5;
+    setLines(scrollY/scrollMax);
+  })
+  console.log(lines)
+})
